8981b8f904bd42e9986595780a40987b378d1837
mugilan <mugilanmsm2000@gmail.com>
2026-08-24 10:15:01 +0530

    Added Ec2 Hostname in CorsConfig

--- src/main/java/com/msm/expenseTrackerBackend/config/CorsConfig.java
+++ src/main/java/com/msm/expenseTrackerBackend/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Replace with your frontend URL
+                .allowedOrigins("http://localhost:5173", "https://api.exptracker.in") // Replace with your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
